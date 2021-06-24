50261fa219c6faca157dd26bca4515b90c634d68
Neeraj Gupta <gneeraj32595@gmail.com>
2021-06-24 11:32:46 +0530

    Update devtools.js

--- js/devtools.js
+++ js/devtools.js
@@ -2,6 +2,9 @@ document.onkeydown = function (e) {
   if (e.keyCode == 123) {
     return false;
   }
+  if(e.metaKey && e.altKey) {
+            return false;
+        }
   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
     return false;
   }
